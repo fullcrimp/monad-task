94fa6b38fcfd7a422dfd36b3e2bbaf3131c4afc2
nik <fullcrimp@protonmail.com>
2017-11-10 11:39:06 +0200

    add kickass refactoring to getNextPosArr function

--- src/index.js
+++ src/index.js
@@ -45,7 +45,10 @@
 /****************************************************************************/
 
 let equal = (firstArr, secondArr) => firstArr.every((element, i) => firstArr[i] === secondArr[i])
-let getNextPosArr = (posArr) => [].concat(...posArr.map((pos) => [[2, 1], [2, -1], [-2, 1], [-2, -1], [1, 2], [1, -2], [-1, 2], [-1, -2]].map((shift) => [pos[0] + shift[0], pos[1] + shift[1]])))
+let getNextPosArr = (posArr) => posArr.reduce((result, pos) => {
+  let shiftCombinations = [1, 2, -1, -2].reduce((acc, val1, i, arr) => [...acc, ...(arr.filter((val2, j) => i !== j && Math.abs(val2) !== Math.abs(val1) ).map(val3 => [val1, val3]))], [])
+  return [...result, ...shiftCombinations.map((shift) => [pos[0] + shift[0], pos[1] + shift[1]])]
+}, [])
 let cleanFromForbiddenMoves = (posArr) => posArr.filter((pos) => pos[0] > 0 && pos[0] < 9 && pos[1] > 0 && pos[1] < 9)
 
 module.exports = function canReach(startPos, finalPos, steps) {
